85810c594cfc0e0eaa2005a90628d0628c383837
deevanych <deevanych@gmail.com>
2023-04-05 02:56:49 +0300

    add new models: delivery method product type

---
+++ src/api/delivery-method/controllers/delivery-method.ts
@@ -0,0 +1,7 @@
+/**
+ * delivery-method controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::delivery-method.delivery-method');
---
+++ src/api/delivery-method/services/delivery-method.ts
@@ -0,0 +1,7 @@
+/**
+ * delivery-method service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::delivery-method.delivery-method');
