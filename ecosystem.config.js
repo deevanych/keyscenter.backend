ce7323da56d2e54ffb6f65b026bcf4485fc590fd
deevanych <deevanych@gmail.com>
2023-04-06 15:26:47 +0300

    change script in ecosystem config for PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name   : "shop-backend",
-    script : "npm",
+    script : "npm run start",
     instances: 4,
     env_production: {
       NODE_ENV: "production"
