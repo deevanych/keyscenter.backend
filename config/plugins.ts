826a39f0107a319bba7fdece4d4dc813cf75d509
deevanych <deevanych@gmail.com>
2023-04-07 13:01:22 +0300

    add redis cache

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,3 +1,42 @@
 export default () => ({
-
+  redis: {
+    enabled: true,
+    config: {
+      connections: {
+        default: {
+          connection: {
+            host: '127.0.0.1',
+            port: 6379,
+            db: 0,
+          },
+          settings: {
+            debug: true,
+          },
+        },
+      },
+    },
+  },
+  "rest-cache": {
+    enabled: true,
+    config: {
+      provider: {
+        name: "redis",
+        options: {
+          max: 32767,
+          connection: "default",
+        },
+      },
+      strategy: {
+        enableEtag: true,
+        enableXCacheHeaders: true,
+        resetOnStartup: true,
+        hitpass: false,
+        keysPrefix: process.env.DOPPLER_ENVIRONMENT,
+        contentTypes: [
+          "api::product.product"
+        ],
+        debug: true
+      },
+    },
+  },
 })
