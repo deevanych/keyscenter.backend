233b475ef4616ea7f2745f146d0913d2b471c6c8
deevanych <deevanych@gmail.com>
2023-04-11 00:49:55 +0300

    add baseurl for product images

--- src/api/product/services/product.ts
+++ src/api/product/services/product.ts
@@ -4,4 +4,33 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::product.product');
+const _url = (imageUrl: string) => new URL(imageUrl, process.env.BASE_URL)
+
+const productWithReplacedImages = (product) => {
+  if (product.images && typeof product.images !== 'undefined') {
+    product.images = product.images.map((image) => {
+      for (const format in image.formats) {
+        image.formats[format] = {
+          ...image.formats[format],
+          url: _url(image.formats[format].url).href
+        }
+      }
+
+      return {
+        ...image,
+        url: _url(image.url).href
+      }
+    })
+
+    return product
+  }
+}
+
+export default factories.createCoreService('api::product.product', ({ strapi }): {} => ({
+  async findOne(id, params) {
+    const product = await strapi.entityService.findOne('api::product.product', id, params)
+
+    return productWithReplacedImages(product)
+  }
+}));
+
