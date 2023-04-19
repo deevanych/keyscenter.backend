01f00ff04576dbc6353a5fa09fe2d7afb78cd6b7
deevanych <deevanych@gmail.com>
2023-04-20 02:19:45 +0300

    add update views

--- src/api/product/controllers/product.ts
+++ src/api/product/controllers/product.ts
@@ -1,7 +1,31 @@
 /**
  * product controller
  */
-
+// @ts-nocheck
 import { factories } from '@strapi/strapi'
+import {up} from "inquirer/lib/utils/readline";
+export default factories.createCoreController('api::product.product', ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id: slug } = ctx.params;
+      const { query } = ctx;
 
-export default factories.createCoreController('api::product.product');
+      const entity = await strapi.db.query('api::product.product').findOne({
+        where: { slug },
+        ...query
+      });
+
+      const updatedEntity = await strapi.db.query('api::product.product').update({
+        where: {slug},
+        data: {
+          views: +entity.views + 1
+        }
+      })
+
+      entity.views = updatedEntity.views
+
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(sanitizedEntity);
+    }
+})
+);
--- src/api/product/routes/product.ts
+++ src/api/product/routes/product.ts
@@ -4,4 +4,6 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::product.product');
+export default factories.createCoreRouter('api::product.product', {
+  only: ['find', 'findOne'], config: undefined
+});
