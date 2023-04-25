f5d83c541dc22fc4ac8aff99632f91eb0cdc3c77
deevanych <deevanych@gmail.com>
2023-04-26 01:55:35 +0300

    add webhook for order payment notification

--- src/api/order/controllers/order.ts
+++ src/api/order/controllers/order.ts
@@ -2,18 +2,22 @@
  * order controller
  */
 
-import { factories } from '@strapi/strapi'
-import { v4 as uuidv4 } from 'uuid';
+import {factories} from '@strapi/strapi'
+import {v4 as uuidv4} from 'uuid';
+import crypto from 'crypto'
+import utils from '@strapi/utils'
 
-export default factories.createCoreController('api::order.order', ({ strapi }) => ({
-  async create(ctx) {
-    const cartItems = ctx.request.body.items
-    const keysIds = []
-    const products = await strapi.entityService.findMany('api::product.product', {
-      fields: ['sale_price'],
-      filters: {
-        $or: ctx.request.body.items.map((item) => { return { id: item.id } })
-      },
+export default factories.createCoreController('api::order.order', ({strapi}) => ({
+    async create(ctx) {
+      const cartItems = ctx.request.body.items
+      const keysIds = []
+      const products = await strapi.entityService.findMany('api::product.product', {
+        fields: ['sale_price'],
+        filters: {
+          $or: ctx.request.body.items.map((item) => {
+            return {id: item.id}
+          })
+        },
     });
 
     for (const item of cartItems) {
@@ -81,7 +85,44 @@ export default factories.createCoreController('api::order.order', ({ strapi }) =
       }
     })
 
-    return order
-  }
-})
+      return order
+    },
+    async orderStatusHook(ctx) {
+      try {
+        const {body} = ctx.request
+        const order_uuid = 'f3b76dec-e057-4142-81e4-1d6ed71a9482'
+        const validationParams = [
+          body.notification_type,
+          body.operation_id,
+          body.amount,
+          body.currency,
+          body.datetime,
+          body.sender,
+          body.codepro,
+          process.env.YOOUMONEY_SECRET,
+          body.label
+        ]
+        const hashInstance = crypto.createHash('sha1')
+        hashInstance.update(validationParams.join('&'))
+        const hexString = hashInstance.digest('hex')
+
+        if (body.sha1_hash !== hexString) {
+          throw 'Hashes are not equals'
+        }
+
+        return await strapi.db.query('api::order.order').update({
+          where: {
+            order_uuid
+          },
+          data: {
+            is_paid: true
+          }
+        })
+      } catch (e) {
+        throw new utils.errors.ForbiddenError(e);
+      }
+
+      return 'success'
+    }
+  })
 );
