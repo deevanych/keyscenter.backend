94e8eaffa5d8cc3fbed27124d4338857d5476b3f
deevanych <deevanych@gmail.com>
2023-04-29 11:17:58 +0300

    change product find service

--- src/api/product/services/product.ts
+++ src/api/product/services/product.ts
@@ -7,19 +7,54 @@
 import {factories} from '@strapi/strapi';
 
 export default factories.createCoreService('api::product.product', ({strapi}) => ({
-    async findOne(slug: string, params: { populate: any }) {
-      return await strapi.db.query('api::product.product').findOne({
-        where: {slug},
-        select: [
-          'title',
-          'price',
-          'salePrice',
-          'slug',
-          'description',
-          'instruction',
-          'views'
-        ],
-        ...params,
+  async find(params: { populate: any }) {
+    const results = await strapi.db.query('api::product.product').findMany({
+      select: [
+        'title',
+        'price',
+        'salePrice',
+        'slug',
+      ],
+      populate: {
+        images: {
+          select: ['formats']
+        },
+        product_category: {
+          select: ['slug', 'title']
+        },
+        product_keys: {
+          select: ['id'],
+          where: {
+            $and: [
+              {
+                published_at: {
+                  $notNull: true
+                }
+              },
+              {
+                order: null
+              },
+            ]
+          }
+        }
+      }
+    })
+
+    return {results}
+  },
+  async findOne(slug: string, params: { populate: any }) {
+    return await strapi.db.query('api::product.product').findOne({
+      ...params,
+      where: {slug},
+      select: [
+        'title',
+        'price',
+        'salePrice',
+        'slug',
+        'description',
+        'instruction',
+        'views'
+      ],
         populate: {
           images: {
             select: ['formats']
