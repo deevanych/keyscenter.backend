7672e38f73769211ed4e7596ebfa6b12947726e9
deevanych <deevanych@gmail.com>
2023-05-14 13:08:18 +0300

    add pages

--- src/api/page/controllers/page.ts
+++ src/api/page/controllers/page.ts
@@ -4,4 +4,14 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::page.page');
+export default factories.createCoreController('api::page.page', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id: slug } = ctx.params
+
+    return strapi.db.query('api::page.page').findOne({
+      where: {
+        slug
+      }
+    })
+  }
+}));
