4694230b9d5c082efab29f5ca01bc6c24dc2bcfa
deevanych <deevanych@gmail.com>
2023-06-28 23:34:31 +0300

    fix cart

--- src/api/cart/content-types/cart/lifecycles.ts
+++ src/api/cart/content-types/cart/lifecycles.ts
@@ -3,18 +3,21 @@ import { type } from 'os';
 export default {
   afterFindOne(event) {
     const {result} = event
-    const {coupons} = result
 
-    if (typeof coupons !== 'undefined' && coupons.length >= 1) {
-      result.sum = coupons.reduce((sum, coupon) => {
-        let discountAmount = coupon.discount
+    if (result) {
+      const {coupons} = result
 
-        if (coupon.discount_type === 'percent') {
-          discountAmount = sum / 100 * discountAmount
-        }
+      if (typeof coupons !== 'undefined' && coupons.length >= 1) {
+        result.sum = coupons.reduce((sum, coupon) => {
+          let discountAmount = coupon.discount
 
-        return sum < 0 ? 0 : sum - discountAmount
-      }, result.sum)
+          if (coupon.discount_type === 'percent') {
+            discountAmount = sum / 100 * discountAmount
+          }
+
+          return sum < 0 ? 0 : sum - discountAmount
+        }, result.sum)
+      }
     }
   }
 }
