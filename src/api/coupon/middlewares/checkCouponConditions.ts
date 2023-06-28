7400c36aa54e3f9dc74b6a053e197a150b5629a5
deevanych <deevanych@gmail.com>
2023-06-29 00:15:03 +0300

    fix non founded coupon handling

--- src/api/coupon/middlewares/checkCouponConditions.ts
+++ src/api/coupon/middlewares/checkCouponConditions.ts
@@ -12,59 +12,67 @@ export default (config, { strapi }: { strapi: Strapi }) => {
     const { code } = ctx.request.body.data
     const { cartId: uuid } = ctx.request.params
 
-    const { max_applies_count, applies_count, can_be_applied_with_other, publishedAt } = await strapi.db.query('api::coupon.coupon').findOne({
-      where: {
-        code
-      },
-      select: [
-        'max_applies_count',
-        'applies_count',
-        'can_be_applied_with_other',
-        'published_at'
-      ]
-    })
-    const { coupons: cartAppliedCoupons } = await strapi.db.query('api::cart.cart').findOne({
-      where: {
-        uuid
-      },
-      select: ['id'],
-      populate: {
-        coupons: {
-          select: ['id', 'can_be_applied_with_other']
-        }
-      }
-    })
-
-
-    if (!publishedAt) {
-      throw new NotFoundError('Промокод не найден');
-    }
-
-    if (+max_applies_count > -1 && +applies_count >= +max_applies_count) {
-      throw new ForbiddenError('Срок действия промокода истек');
-    }
-
-    if (!can_be_applied_with_other) {
-      if (cartAppliedCoupons.length > 0) {
-        throw new ForbiddenError('Промокод не может быть применен с другими промокодами');
-      }
-    }
-
-    const existUniqueCoupon = cartAppliedCoupons.find((coupon) => !coupon.can_be_applied_with_other)
-
-    if (existUniqueCoupon) {
-      await strapi.db.query('api::cart.cart').update({
+    try {
+      const {
+        max_applies_count,
+        applies_count,
+        can_be_applied_with_other,
+        publishedAt
+      } = await strapi.db.query('api::coupon.coupon').findOne({
+        where: {
+          code
+        },
+        select: [
+          'max_applies_count',
+          'applies_count',
+          'can_be_applied_with_other',
+          'published_at'
+        ]
+      })
+      const {coupons: cartAppliedCoupons} = await strapi.db.query('api::cart.cart').findOne({
         where: {
           uuid
         },
-        data: {
+        select: ['id'],
+        populate: {
           coupons: {
-            disconnect: [existUniqueCoupon.id]
+            select: ['id', 'can_be_applied_with_other']
           }
         }
       })
-    }
 
-    await next();
+      if (!publishedAt) {
+        throw new NotFoundError('Промокод не найден');
+      }
+
+      if (+max_applies_count > -1 && +applies_count >= +max_applies_count) {
+        throw new ForbiddenError('Срок действия промокода истек');
+      }
+
+      if (!can_be_applied_with_other) {
+        if (cartAppliedCoupons.length > 0) {
+          throw new ForbiddenError('Промокод не может быть применен с другими промокодами');
+        }
+      }
+
+      const existUniqueCoupon = cartAppliedCoupons.find((coupon) => !coupon.can_be_applied_with_other)
+
+      if (existUniqueCoupon) {
+        await strapi.db.query('api::cart.cart').update({
+          where: {
+            uuid
+          },
+          data: {
+            coupons: {
+              disconnect: [existUniqueCoupon.id]
+            }
+          }
+        })
+      }
+
+      await next();
+    } catch (_) {
+      throw new NotFoundError('Промокод не найден');
+    }
   };
 };
