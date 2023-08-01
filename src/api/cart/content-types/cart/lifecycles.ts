8576ccec8fb8e3468288b830a00705c626510e45
deevanych <deevanych@gmail.com>
2023-08-01 20:51:03 +0300

    fix coupon non positive total sum

--- src/api/cart/content-types/cart/lifecycles.ts
+++ src/api/cart/content-types/cart/lifecycles.ts
@@ -1,23 +1,22 @@
-import { type } from 'os';
-
 export default {
   afterFindOne(event) {
-    const {result} = event
+    const { result } = event;
 
     if (result) {
-      const {coupons} = result
+      const { coupons } = result;
 
-      if (typeof coupons !== 'undefined' && coupons.length >= 1) {
+      if (typeof coupons !== "undefined" && coupons.length >= 1) {
         result.sum = coupons.reduce((sum, coupon) => {
-          let discountAmount = coupon.discount
+          console.log(sum);
+          let discountAmount = coupon.discount;
 
-          if (coupon.discount_type === 'percent') {
-            discountAmount = sum / 100 * discountAmount
+          if (coupon.discount_type === "percent") {
+            discountAmount = (sum / 100) * discountAmount;
           }
 
-          return sum < 0 ? 0 : sum - discountAmount
-        }, result.sum)
+          return sum <= 0 ? 0 : sum - discountAmount;
+        }, result.sum);
       }
     }
-  }
-}
+  },
+};
